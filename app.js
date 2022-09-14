b0dd99196b1c7230d4a8025386fd9e2f6bee3f92
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-09-14 12:40:17 +0530

    added local file to github
    commit

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import ejs from "ejs";
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import postRouter from "./routes/post-routes";
+import userRouter from "./routes/user-routes.mjs";
+import auth from "./routes/auth.mjs";
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use("/user",userRouter);
+app.use("/post",postRouter);
+
+
+mongoose.connect(process.env.MongoDB_Connection_Link, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("Connected to the db");
+}).catch((err) => {
+    console.log(err);
+});
+
+app.get("/", auth, function(req,res){
+  if(res.statusCode != 401){
+    console.log("in / route");
+    res.redirect("/user/home");
+  }
+   res.render("welcome_page");
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/thankyou", function(req, res){
+  res.render("thankyou");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
+});
+
+
+
+
+//Bookie 
